750ad64c0c562b74c23f8024e098c76ed6990e8a
Pratik Verma <157415997+pvatokahu@users.noreply.github.com>
2025-02-24 11:47:12 -0800

    chatbot code without instrumentation

--- src/app/api/coffeechat/route.ts
+++ src/app/api/coffeechat/route.ts
@@ -6,7 +6,6 @@ export const POST = async (request: Request) => {
 
   const sessionId = request.headers.get('X-Session-Id');
   logger.info(`Processing request for session: ${sessionId}`);
-  process.env["MONOCLE_S3_KEY_PREFIX_CURRENT"] = sessionId ? sessionId + "__" : ""
 
   const json = await request.json()
   console.log("Request:", json.message);
